64ddb36dac0991ee9207b7aaf359dc0eebd5893d
zenin.a.s <zenin.a.s@effective-group.ru>
2023-06-08 15:25:32 +0300

    Первый тест

---
+++ one.py
@@ -0,0 +1,5 @@
+
+print("Привет", end='!!! \n',)
+print("Я учусь программировать на Python!!!" ,end = ' \n')
+print('', 'P', 'Y', 'T', 'H', 'O', 'N', sep='***',end = '*** \n')
+print("***Version Python 3.10",end='***' )
