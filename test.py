4871fd50bb61723460e7c70d9563ef66605e29fb
zenin.a.s <zenin.a.s@effective-group.ru>
2023-12-04 15:59:00 +0300

    Урок .11 - Модули

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-x = int(input('Введите число для расчета фактариала: '))
+from Urok_11_modul1 import obem_kuba
+from Urok_11_Modul2 import ploshad_trapec
 
-faktorial = 1
-perebor_chisel = 1
-while perebor_chisel <= x:
-    faktorial *= perebor_chisel
-    perebor_chisel += 1
-print('факториал числа: ', x, 'равен: ', faktorial)
+f = int(input('Для рассчета объема куба введите длинну его стороны: '))
+print(f'Объем куба равен {obem_kuba(f)}')
+
+a = int(input('Для рассчета площади трапеции введите первую длинну стороны: '))
+b = int(input('Введите вторую длинну стороны: '))
+h = int(input('Введите длинну высоты: '))
+print(f'По вышим параметрам площадь трапеции равна {ploshad_trapec(a,b,h)}')
